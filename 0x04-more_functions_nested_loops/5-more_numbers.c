1cf8309385cde575ae50693f5aa3bb71a097420a
NyeraGbr <34568106+nyera@users.noreply.github.com>
2023-08-17 22:41:44 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,15 @@
 */
 void more_numbers(void)
 {
-  int j;
-    int i;
+  int j, i;
   for (j = 0; j <= 10; j++)
     {  
          for (i = 0; i < 15 ; i++)
     {
-          if (j <= 10)
+          if (i <= 10)
           
-            	_putchar(j / 10 + '0');
-		        	_putchar(j % 10 + '0');
+            	_putchar(i / 10 + '0');
+		        	_putchar(i % 10 + '0');
            
     }  
        _putchar('\n');
