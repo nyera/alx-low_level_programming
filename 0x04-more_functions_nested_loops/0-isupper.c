06b98874c1fa804db66629ab4e6ceeff04789ad6
NyeraGbr <34568106+nyera@users.noreply.github.com>
2023-08-17 20:03:39 +0300

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * 0. isupper
+ *Returns 1 if c is uppercase
+ *Returns 0 otherwise
+ */
+int _isupper(int c)
+{
+
+
+  if (c >= 'A')
+  {
+    return (1);
+  }
+  else if (c <= 'Z')
+  {
+    return (0);
+  }
+      
+}
