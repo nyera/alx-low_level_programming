0bebd9b187a0b4ad46177dd7fb363f68a6ebdba8
nyera <nyeragbr@hotmail.com>
2023-09-11 19:05:08 +0300

    commit

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,19 +12,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ar;
-	unsigned int ar_size, i;
+	char *ptr;
+	unsigned int count;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ar_size = nmemb * size;
-	ar = malloc(ar_size);
-	if (ar == NULL)
-		return (NULL);
-	while (i < ar_size)
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
 	{
-		ar[i] = 0;
-		i++;
+		return (NULL);
 	}
-	return (ar);
+	count = 0;
+	while (count < nmemb * size)
+	{
+		ptr[count] = 0;
+		count++;
+	}
+	return (ptr);
 }
