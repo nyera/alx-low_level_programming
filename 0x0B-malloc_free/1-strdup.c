fae7c8920b93d4cae8b30a0af2bfd72337e47ffe
nyera <nyeragbr@hotmail.com>
2023-09-11 16:36:48 +0300

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,14 +15,13 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
-	for (i=0; str[i] != '\0'; i++)
-	{
-		a = malloc(i * sizeof(*a) +1);
-	}
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	a = malloc(i * sizeof(*a) + 1);
 	if (a == NULL)
-	{
 		return (NULL);
-	}
 
 	for (c = 0; c < i; c++)
 		a[c] = str[c];
